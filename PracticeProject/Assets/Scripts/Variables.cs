4025c6f455d4d9e6ff3464d4d64992aaea0459bd
Ahmet Turan Keçe <94369527+ahmettturan@users.noreply.github.com>
2024-10-07 03:04:15 +0300

    2024.10.7 V2
    Variables done!

--- PracticeProject/Assets/Scripts/Variables.cs
+++ PracticeProject/Assets/Scripts/Variables.cs
@@ -1,16 +1,47 @@
 using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System;
 
 public class Variables : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+  /*
+     string  // metinsel verileri içerir.
+     int     // sayısal değerleri
+     float // kayan sayılar 3.1014
+     double // daha büyük kayan sayı 3.21414124241
+     Boolean // herhangi bir şeyi aktif veya pasif olmasını kontrol eder. true / false
+     
+         */
+
+    /*
+            // class içerisinde değer tanımlama
+        string metnim = "Merhaba Olcay";
+        int yas = 80;
+        float kusurat = 3.10f;
+        double kusurat2 = 3.2120;
+        bool acikmi = false;
+           // class içerisinde değer tanımlama
+           */
+    // sadece değişkenlerin isimlerini ve türlerini belirliyorum
+    string metnim;
+    int yas;
+    float kusurat;
+    double kusurat2;
+    bool acikmi;
+    // sadece değişkenlerin isimlerini ve türlerini belirliyorum
+
     void Start()
     {
-        
+        // işlemler yaptım sorgular yaptım ve değişkenin değeri şu an oluştu
+
+        metnim = "iceriden";
+
+        Debug.Log(metnim);
+        Debug.Log(yas);
+        Debug.Log(kusurat);
+        Debug.Log(kusurat2);
+        Debug.Log(acikmi);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
