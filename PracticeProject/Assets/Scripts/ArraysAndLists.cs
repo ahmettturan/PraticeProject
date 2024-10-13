cadda1ef71e9b474484bb89d8ee277d01d36b588
ahmetturan <94369527+ahmettturan@users.noreply.github.com>
2024-10-13 21:43:13 +0300

    2024.13.10

--- PracticeProject/Assets/Scripts/ArraysAndLists.cs
+++ PracticeProject/Assets/Scripts/ArraysAndLists.cs
@@ -4,6 +4,22 @@ public class ArraysAndLists : MonoBehaviour
 {
     void Start()
     {
-               
+       /* int[] sayilarim = new int[5];
+        string[] yazilarim = new string[2]; */
+
+        /* int[] sayilarim = new int[] {1,2,3,4,5,6,9};
+
+        Debug.Log(sayilarim[2]); */
+
+        string[][] arabalar = new string[3][];
+
+        arabalar[0] = new string[] {"Bmw","Audi","Seat"};
+        arabalar[1] = new string[] {"2010" ,"2009" ,"2014"};
+        arabalar[2] = new string[] {"140hp","100hp","90hp"};
+
+        Debug.Log(arabalar[0][1]);
+        Debug.Log(arabalar[1][0]);
+
+        
     }
 }
